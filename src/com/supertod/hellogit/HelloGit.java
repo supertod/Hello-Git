1aa65c6c843f4986a6891a83735c7dec5ca36282
Thomas Davidsson <thomasdavidsson@supertod.se>
2011-09-05 17:33:54 +0200

    added src directory

---
+++ src/com/supertod/hellogit/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.supertod.hellogit;
+
+/**
+ * @author Supertod
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
